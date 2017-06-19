1bcc7549b27405838c23183d8fb59aa3be3f69c1
Krystian Babilinski <kbabilinski@gmail.com>
2017-06-19 11:03:42 -0400

    refactor PictureFrame class

--- Assets/ARGallery/Scripts/Data/PictureFrame.cs
+++ Assets/ARGallery/Scripts/Data/PictureFrame.cs
@@ -7,13 +7,25 @@ using UnityEngine;
 public class PictureFrame
 {
 
-    public string id;
+    private string id;
     public GameObject PictureFrameObject;
     public Texture Image;
 
     public PictureFrame CompareFrameById(string Id)
     {
         if (id == Id)
+        {
             return this;
+        }
+        else
+        {
+            return null;
+        }
+     
+    }
+
+    public PictureFrame(string id)
+    {
+        this.id = id;
     }
 }
