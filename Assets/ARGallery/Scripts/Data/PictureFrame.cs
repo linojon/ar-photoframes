904a38c73ed8442e776baaa6d4f93dd374d80b26
Krystian Babilinski <kbabilinski@gmail.com>
2017-06-19 10:59:50 -0400

    added code to data classes

--- Assets/ARGallery/Scripts/Data/PictureFrame.cs
+++ Assets/ARGallery/Scripts/Data/PictureFrame.cs
@@ -1,16 +1,13 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿
+
+
 using UnityEngine;
 
-public class PictureFrame : MonoBehaviour {
+[System.Serializable]
+public class PictureFrame
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public string id;
+    public GameObject PictureFrameObject;
+    public Texture Image;
 }
