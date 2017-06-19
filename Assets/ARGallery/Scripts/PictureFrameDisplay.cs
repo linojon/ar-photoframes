a6be70272e1231d4584c5bc2b346ffe9824d4b5c
Krystian Babilinski <kbabilinski@gmail.com>
2017-06-19 11:15:41 -0400

    created picture frame script

---
+++ Assets/ARGallery/Scripts/PictureFrameDisplay.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+[RequireComponent(typeof(ImageTargetBehaviour))]
+public class PictureFrameDisplay : MonoBehaviour
+{
+
+    public Texture ImageTexture;
+    //The application could be optimized by using object pooling
+    public GameObject FrameSpawnPoint;
+
+    [SerializeField]
+    private PictureFrame pictureFrame;
+    private ImageTargetBehaviour imageTargetBehaviour;
+
+    void Start()
+    {
+        imageTargetBehaviour = GetComponent<ImageTargetBehaviour>();
+        pictureFrame = new PictureFrame(imageTargetBehaviour.name);
+    }
+}
